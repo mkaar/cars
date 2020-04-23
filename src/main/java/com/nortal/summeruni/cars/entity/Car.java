40b5d95027c4d54329814e84c332403d9a68390c
Marko Kaar <marko.kaar@nortal.com>
2020-04-23 11:31:05 +0300

    Fixed generated value to support manual insertions

--- src/main/java/com/nortal/summeruni/cars/entity/Car.java
+++ src/main/java/com/nortal/summeruni/cars/entity/Car.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 public class Car {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String brand;
     private String model;
